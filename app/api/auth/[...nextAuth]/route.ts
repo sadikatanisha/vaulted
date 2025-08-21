8e29c550ab8e05947e94238984f537119bbd56b8
unknown <sadikatanisha@gmail.com>
2025-08-22 00:56:34 +0600

    changes

--- app/api/auth/[...nextAuth]/route.ts
+++ app/api/auth/[...nextAuth]/route.ts
@@ -1,11 +1,10 @@
-// import handlers from "@/auth"
-// export const { GET, POST } = handlers
+
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
-
-const prisma = new PrismaClient()
+import prisma from "@/lib/db"
+// const prisma = new PrismaClient()
 
 export const authOptions = {
   providers: [
